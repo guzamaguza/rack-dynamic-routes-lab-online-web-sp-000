3c47b9ac37aad03e71db9fa7129b42911694a224
Mark A Guza <guzamaguza@gmail.com>
2020-04-30 00:47:01 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,13 +7,13 @@ class Application
       req = Rack::Request.new(env)
 
       if req.path.match(/items/)
-        requested_item = req.path.split("/items/").last 
+        requested_item = req.path.split("/items/").last
         detected_item = @@items.detect{|item| item.name = requested_item}
-        if detected_item 
+        if detected_item
           resp.write item.price
-        else 
-          resp.status = 400 
-          resp.write = "Item Not Found" 
+        else
+          resp.status = 400
+          resp.write = "Item Not Found"
         end
       else
         resp.status = 404
