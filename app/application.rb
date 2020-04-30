969ce158391ae61180cbb221c8d4f4a684f96cc4
Mark A Guza <guzamaguza@gmail.com>
2020-04-30 00:46:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,16 +1,19 @@
 class Application
 
-  #@@items = ["Apples","Carrots","Pears"]
+  @@items = []
 
   def call(env)
       resp = Rack::Response.new
       req = Rack::Request.new(env)
 
       if req.path.match(/items/)
-        Item.each do |item|
-          if item == env
-          resp.write "#{item}\n"
-          end
+        requested_item = req.path.split("/items/").last 
+        detected_item = @@items.detect{|item| item.name = requested_item}
+        if detected_item 
+          resp.write item.price
+        else 
+          resp.status = 400 
+          resp.write = "Item Not Found" 
         end
       else
         resp.status = 404
