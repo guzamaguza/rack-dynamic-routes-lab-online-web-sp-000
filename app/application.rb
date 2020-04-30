2be3c8ee204ab44d9ff210ed6b9be21e5a91a1de
Mark A Guza <guzamaguza@gmail.com>
2020-04-30 00:39:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,46 +1,21 @@
-def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
+class Application
 
-    if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
-    else
-      resp.status = 404
-      resp.write "Path Not Found"
-    end
+  #@@items = ["Apples","Carrots","Pears"]
 
-      if @@cart == []
-        resp.write "Your cart is empty"
-      else
-        @@cart.each do |item|
+  def call(env)
+      resp = Rack::Response.new
+      req = Rack::Request.new(env)
+
+      if req.path.match(/items/)
+        Item.each do |item|
+          if item == env 
           resp.write "#{item}\n"
+          else
+            resp.status = 404 
+            resp.write = "Path Not Found" 
+          end
         end
-      end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    elsif req.path.match(/add/)
-      item = req.params["item"]
-      if @@items.include?(item)
-        @@cart << item
-        resp.write "added #{item}"
-      else
-        resp.write "We don't have that item"
-      end
-    else
-      resp.write "Path Not Found"
-    end
-
-    resp.finish
+      resp.finish
   end
 
-  def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
-    end
-  end
 end
