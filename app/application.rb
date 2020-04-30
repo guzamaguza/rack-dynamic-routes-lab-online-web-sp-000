7effe62d89b135b9eb7e66a995bf3d4bf5fc4167
Mark A Guza <guzamaguza@gmail.com>
2020-04-30 00:53:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
         requested_item = req.path.split("/items/").last
         detected_item = @@items.detect{|i| i.name == requested_item}
         if detected_item
-          resp.write item.price
+          resp.write detected_item.price
         else
           resp.status = 400
           resp.write "Item not found"
